c1c3c2ea884c98f06096ab890edc25477a49977b
nguyenlinh7991 <ng.linh7991@gmail.com>
2019-09-25 12:10:26 +0200

    update final

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cours-imie'));
+app.use(express.static(__dirname + '/dist/cours-IMIE'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/cours-imie/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cours-IMIE/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
